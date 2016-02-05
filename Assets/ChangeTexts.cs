31747c17ce0acef201a0fa9545933c42d765c684
Igor Frias Vieira <igor.soad@gmail.com>
2016-02-05 11:13:43 -0200

    Change text

---
+++ Assets/ChangeTexts.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTexts : MonoBehaviour {
+
+	public string[] texts;
+	private TextMesh textObject;
+
+	// Use this for initialization
+	void Start () {
+		textObject = GetComponent<TextMesh>();
+		ChangeText ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.Return)) {
+			ChangeText ();
+		}
+	}
+
+	string GetARandomText () {
+		int i = Random.Range(0,(texts.Length - 1));
+		return texts[i];
+	}
+
+	void ChangeText () {
+		textObject.text = GetARandomText ();
+	}
+}
